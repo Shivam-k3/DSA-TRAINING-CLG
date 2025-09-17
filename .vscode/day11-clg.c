ce3adb5a0b93f0070c44de232be7e175815ab3cd
Shivam-k3 <aishivamsingh01@gmail.com>
2025-09-17 20:22:00 +0530

    Push source files without exe files

--- .vscode/day11-clg.c
+++ .vscode/day11-clg.c
@@ -1,56 +1,56 @@
-// // //celsius to fahrenheit
-// #include <stdio.h>
-// int main()
-// {
-//     float celsius, fahrenheit, kelvin;
-//     printf("Enter temperature in Celsius: ");
-//     scanf("%f", &celsius);
-//     fahrenheit = (celsius * 9/5) + 32;
-//     kelvin = celsius + 273.15;
-// printf("Temperature in Fahrenheit: %f\nTemperature in Kelvin: %f\n", fahrenheit, kelvin);
-//     return 0;
-// }
-// // wap to assign grades of a student based on marks obtained in 5 subjects calculate percentage and grade
-// #include <stdio.h>
-// int main()
-// {
-// int marks[5],sum=0;
-// float percentage;
-// char grade;
-// for(int i=0;i<5;i++)
-// {
-//     printf("enter the marks of subject %d=",i+1);
-//     scanf("%d",&marks[i]);
-//     sum+=marks[i];
+// //celsius to fahrenheit
+#include <stdio.h>
+int main()
+{
+    float celsius, fahrenheit, kelvin;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9/5) + 32;
+    kelvin = celsius + 273.15;
+printf("Temperature in Fahrenheit: %f\nTemperature in Kelvin: %f\n", fahrenheit, kelvin);
+    return 0;
+}
+// wap to assign grades of a student based on marks obtained in 5 subjects calculate percentage and grade
+#include <stdio.h>
+int main()
+{
+int marks[5],sum=0;
+float percentage;
+char grade;
+for(int i=0;i<5;i++)
+{
+    printf("enter the marks of subject %d=",i+1);
+    scanf("%d",&marks[i]);
+    sum+=marks[i];
 
-// }
-// percentage=sum/5.0;
-// if(percentage>=60 && percentage<=100)
-// {
-//    printf("Grade='A'\n");
-// }
-// else if(percentage>=50 && percentage<60)
-// {
-//     printf("Grade='B'\n");
-// }
-// else if(percentage>=40 && percentage<50)
-// {
-//     printf("Grade='C'\n");
-// }
-// else if(percentage>=30 && percentage<40)
-// {
-//     printf("Grade='D'\n");
-// }
-// else if(percentage<=30 && percentage>=10)
-// {
-//     printf("Grade='E'\n");
-// }
-// else{
-//     printf("failed\n");
-// }
-// printf("percentage=%f\n",percentage);
-// printf("sum=%d\n",sum);
-// }
+}
+percentage=sum/5.0;
+if(percentage>=60 && percentage<=100)
+{
+   printf("Grade='A'\n");
+}
+else if(percentage>=50 && percentage<60)
+{
+    printf("Grade='B'\n");
+}
+else if(percentage>=40 && percentage<50)
+{
+    printf("Grade='C'\n");
+}
+else if(percentage>=30 && percentage<40)
+{
+    printf("Grade='D'\n");
+}
+else if(percentage<=30 && percentage>=10)
+{
+    printf("Grade='E'\n");
+}
+else{
+    printf("failed\n");
+}
+printf("percentage=%f\n",percentage);
+printf("sum=%d\n",sum); 
+}
 // // wap to check wheather a  given number is even or odd using switch case
 #include <stdio.h>
 int main()
--- .vscode/day6-clg.c
+++ .vscode/day6-clg.c
@@ -1,36 +1,36 @@
-// //finding length of string without usin len finction
-// #include <stdio.h>
+//finding length of string without usin len finction
+#include <stdio.h>
 
-// int main() {
-//     char str[100];
-//     int i = 0;
+int main() {
+    char str[100];
+    int i = 0;
 
-//     printf("Enter a string: ");
-//     fgets(str, sizeof(str), stdin);
-//     while (str[i] != '\0' && str[i] !='\n')
-//     {
-//         i++;
-//     }
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[i] != '\0' && str[i] !='\n')
+    {
+        i++;
+    }
 
-//     printf("Length of the string: %d\n", i);
-// }
-// //reverse the string without string method
-// #include <stdio.h>
+    printf("Length of the string: %d\n", i);
+}
+//reverse the string without string method
+#include <stdio.h>
 
-// int main() {
-//     char str[100];
-//     int i = 0, len = 0;
-//      printf("Enter a string: ");
-//     fgets(str, sizeof(str), stdin);
-//     while (str[len] != '\0' && str[len] != '\n') {
-//         len++;
-//     }
-//     printf("Reversed string: ");
-//     for (i = len - 1; i >= 0; i--) {
-//         printf("%c", str[i]);
-//     }
-//     printf("\n");
-//     return 0;
-// }
+int main() {
+    char str[100];
+    int i = 0, len = 0;
+     printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[len] != '\0' && str[len] != '\n') {
+        len++;
+    }
+    printf("Reversed string: ");
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+    return 0;
+}
 
-// // using string method
+// using string method
